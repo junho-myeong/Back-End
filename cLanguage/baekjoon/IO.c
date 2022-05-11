3809286d216c1cfc90d257d88c4b768e007fcd59
junho-myeong <wnsgh9166@naver.com>
2022-05-11 13:20:39 +0900

    baekjoon io

--- cLanguage/baekjoon/IO.c
+++ cLanguage/baekjoon/IO.c
@@ -38,7 +38,7 @@ int main() {
 	scanf_s("%lf%lf", &x, &y);
 	printf("%.13lf", x / y);
 
-  // 10998번
+  // 10869번
 	int x;
 	int y;
 
@@ -49,9 +49,34 @@ int main() {
 	printf("%d\n", x / y);
 	printf("%d\n", x % y);
 
-  // 10998번
+  // 10926번
 	char str[50];
 
 	scanf_s("%s", str, sizeof(str));
 	printf("%s??!\n", str);
+
+  // 10998번
+	int year;
+	const int YEAR1 = 543;
+	scanf("%d", &year);
+	printf("%d", year - YEAR1);
+
+  // 10430번
+	int a, b, c;
+	
+	scanf("%d%d%d", &a, &b, &c);
+	printf("%d\n",(a+b)%c);
+	printf("%d\n",((a%c) + (b%c))%c);
+	printf("%d\n",(a*b)%c);
+	printf("%d\n", ((a % c) * (b % c)) % c);
+
+  // 2588번
+	int a, b;
+	
+	scanf_s("%d\n%d", &a, &b);
+
+	printf("%d\n", a * (b%10));
+	printf("%d\n", a * ((b%100)/10));
+	printf("%d\n", a * (b/100));
+	printf("%d\n", a * b);
 }
